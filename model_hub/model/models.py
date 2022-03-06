5518fc86f9ac924f44286890935fd604274b1019
Saikat Kanjilal <sxk1969@hotmail.com>
2022-03-05 16:21:56 -0800

    flake8 for models

--- model_hub/model/models.py
+++ model_hub/model/models.py
@@ -14,16 +14,16 @@ async def all_models(db: DatabaseManager = Depends(get_database)):
 
 
 @router.get("/one/model")
-async def one_model(model_url: str, db: DatabaseManager = Depends(get_database)):
+async def one_model(model_url: str,
+                    db: DatabaseManager = Depends(get_database)):
     model = await db.get_model(model_url=model_url)
     return model
 
 
 @router.put("/{model_url}")
-async def update_model(
-    model_url: str, model: Model,
-        db: DatabaseManager = Depends(get_database)
-):
+async def update_model(model_url: str,
+                       model: Model,
+                       db: DatabaseManager = Depends(get_database)):
     post = await db.update_model(model=model, model_url=model_url)
     return post
 
