0e61e338b121fe7c1bcb1c61991578374470fd3b
Saikat Kanjilal <sxk1969@hotmail.com>
2022-03-05 16:19:53 -0800

    flask8 fixes

--- model_hub/db/impl/mongo_manager.py
+++ model_hub/db/impl/mongo_manager.py
@@ -32,12 +32,13 @@ class MongoManager(DatabaseManager):
         return models_list
 
     async def get_model(self, model_url: str) -> Model:
-        if (model_q := await self.db.models.\
+        if (model_q := await self.db.models.
                 find_one({"model_url": model_url})) is not None:
             return Model(**model_q, id=model_q["_id"])
         else:
-            raise HTTPException\
-                (status_code=404, detail=f"Model {model_url} not found")
+            raise HTTPException(status_code=404,
+                                detail=f"Model "
+                                       f"{model_url} not found")
 
     async def delete_model(self, model_url: str):
         await self.db.models.delete_one({"model_url": ObjectId(model_url)})
